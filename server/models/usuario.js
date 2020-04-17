91127b18ded3d56d3dab309b093b34997ec843c2
CristianX <thecristianx@hotmail.com>
2020-04-17 14:53:39 -0500

    Implementando el modelo de usuario en models/usuario.js y exportandolo

---
+++ server/models/usuario.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// Obteniendo esquemas de mongoose
+let Schema = mongoose.Schema;
+
+// Declarando esquema
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+
+    },
+
+    email: {
+        type: String,
+        required: [true, 'El correo es necesario']
+    },
+
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        default: 'USER_ROLE'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+
+
+});
+
+// Se exporta el modelo Usuario con la funcionalidad de usuarioSchema
+module.exports = mongoose.model('Usuario', usuarioSchema);

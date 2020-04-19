1ed819f8ca8e3cef0b96d147f86f0582c179e309
CristianX <thecristianx@hotmail.com>
2020-04-19 17:38:42 -0500

    Implementando el post de login (login normal) en routes/login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -12,9 +12,46 @@ const Usuario = require('../models/usuario');
 
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true,
+
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            // Error 500 es un internal server error, error interno del servidor
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: '(Usuario) o contraseña incorrectos'
+                }
+            });
+        }
+
+        // Comparando contraseña ingresada con la contraseña grabada en bcrypt
+        // Regresa un true si hacen match
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o (contraseña) incorrectos'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            toke: '123'
+        });
+
     });
+
 });
 
 

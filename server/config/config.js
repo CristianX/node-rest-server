a9e926c7963de096425251b1d709fd0cd92f3e0b
CristianX <thecristianx@hotmail.com>
2020-04-18 20:04:41 -0500

    Implementando una variable personalizada de heroku para oculpar la url de mongo atlas

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://CristianX:bh14GoHADQRScdHA@cluster0-bohxf.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 // Variable URLDB personalizada

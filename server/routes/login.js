42710a9dfd19d9f22ad529b131f2b0f5eb37a5fd
CristianX <thecristianx@hotmail.com>
2020-04-20 17:49:47 -0500

    Creando usuario personalizado en base a las credenciales de Google, implementado en las rutas de login

--- server/routes/login.js
+++ server/routes/login.js
@@ -81,25 +81,113 @@ async function verify(token) {
     const payload = ticket.getPayload();
 
     // Payload recibe todos los datos del usuario
-    console.log(payload.name);
-    console.log(payload.email);
-    console.log(payload.picture);
+    // console.log(payload.name);
+    // console.log(payload.email);
+    // console.log(payload.picture);
+
+    // Retornando dats segun el modelo
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    };
 }
 
 
 
 // Login con Google
-app.post('/google', (req, res) => {
+app.post('/google', async(req, res) => {
 
     let token = req.body.idtoken;
 
+    // catch para atrapar el error
+    // Se manda a llamar la función de google de verify y se obtiene la variable googleUser con la información del usuario
+    let googleUser = await verify(token).catch(err => {
+        return res.status(403).json({
+            ok: false,
+            err
+        });
+    });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            // Error 500 es un internal server error, error interno del servidor
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe usar su autenticación normal'
+                    }
+                });
+            } else {
+                // Generando token con jason web token (Generando Payload) retorna el token
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN }); // 60 * 60 * 24 * 30 es decir que expira en 30 días
+
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+
+
+            }
+        } else {
+            // Si el usuario no existe en la bdd se crera un nuevo usuario
+            let usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            // El password es obligatorio así que se usa esto, la carita feliz nunca hace match
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    // Error 500 es un internal server error, error interno del servidor
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                // Generando token con jason web token (Generando Payload) retorna el token
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN }); // 60 * 60 * 24 * 30 es decir que expira en 30 días
+
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+
+            });
+        }
+
+
+
+    });
 
     // llamando verify
-    verify(token);
+    // verify(token);
 
-    res.json({
-        token
-    })
+    // res.json({
+    //     usuario: googleUser
+    // });
 
 });
 
